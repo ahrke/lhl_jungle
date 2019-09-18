68b99938026c98cdfa5adcbf379cb1b595eb3f63
ahrke <david.duy.minh@gmail.com>
2019-09-18 18:26:32 +0000

    add product data to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin[:username], password: Rails.configuration.admin[:password]
 
   def show
+    @category = Category.all
+    @product = Product.all
   end
 end

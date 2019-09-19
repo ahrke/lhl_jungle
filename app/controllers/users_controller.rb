085b5e30e46659d345d959b9dd902ddd1798af3a
ahrke <david.duy.minh@gmail.com>
2019-09-19 18:15:03 +0000

    add user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,24 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new
-    @user.first_name = params["user"][:first_name]
-    @user.last_name = params["user"][:first_name]
-    @user.password = params["user"][:first_name]
-    @user.email = params["user"][:first_name]
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to :new
+    end
   end
 
-  def log_in
+  def login
+  end
+
+  def logout
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  
+  has_secure_password
+  
   validates :first_name, 
             :last_name, 
             :password, 
